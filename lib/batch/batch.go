39c230f50a750730ada5a5ca6bea90b8550d98f8
a.volkovytskaya <a.volkovytskaya@admitad.com>
2022-07-17 22:02:33 +0300

    add limit

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -15,19 +15,25 @@ func getOne(id int64) user {
 }
 
 func getBatch(n int64, pool int64) (res []user) {
+
 	var wg sync.WaitGroup
 	var m sync.Mutex
+	sem := make(chan struct{}, pool)
 	var i int64 = 0
+
 	for i = 0; i < n; i++ {
 		wg.Add(1)
+		sem <- struct{}{}
 		go func(id int64) {
 			defer wg.Done()
 			u := getOne(id)
+			<-sem
 			m.Lock()
 			res = append(res, u)
 			m.Unlock()
 		}(i)
 	}
+
 	wg.Wait()
 	return res
 }
