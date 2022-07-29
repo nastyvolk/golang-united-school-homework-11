c07df58fe2167069e3c6e56c6b3d43f462599981
nastyvolk <99589062+nastyvolk@users.noreply.github.com>
2022-07-29 10:42:58 +0300

    Update batch.go

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -18,16 +18,16 @@ func getBatch(n int64, pool int64) (res []user) {
 
 	var wg sync.WaitGroup
 	var m sync.Mutex
-	sem := make(chan struct{}, pool)
+	p := make(chan struct{}, pool)
 	var i int64 = 0
 
 	for i = 0; i < n; i++ {
 		wg.Add(1)
-		sem <- struct{}{}
+		p <- struct{}{}
 		go func(id int64) {
 			defer wg.Done()
 			u := getOne(id)
-			<-sem
+			<-p
 			m.Lock()
 			res = append(res, u)
 			m.Unlock()
